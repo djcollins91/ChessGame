1df1b584385516633aba216e092c81461b26e02c
Dan Collins <cdan200@icloud.com>
2024-08-09 16:03:45 -0600

    got all tests to pass for WR

---
+++ tests/white_pieces_tests/rook/test_WR_move_with_piece_in_way.py
@@ -0,0 +1,48 @@
+import unittest
+from board import Board
+from white_pieces.rook.white_rook import White_Rook
+from black_pieces.rook.black_rook import Black_Rook
+
+class TestBoard(unittest.TestCase):
+
+    def setUp(self):
+        self.board = Board()
+        self.initialize_pieces(self.board)
+
+    def initialize_pieces(self, board):
+
+        board_width = range(Board.getWIDTH())
+        # Create white and black rooks with specific classes
+        # Create white and black rooks with specific classes
+        white_rooks = [White_Rook('WR') for _ in board_width]
+        black_rooks = [Black_Rook('BR') for _ in board_width]
+
+        #places the white_rooks
+        for i, rook in enumerate(white_rooks):
+            if(i == 0) or (i == 7):
+                board.place_piece(rook, i, 0)
+                
+        #places the black_rooks
+        for i, rook in enumerate(black_rooks):
+            if (i == 0) or (i == 7):
+                board.place_piece(rook, i, 7)
+
+    def test_valid_move_forward(self):
+        # Test 7: Makes sure you can't move when piece is in the way
+        from_x, from_y = 0, 0
+        self.board.place_piece(White_Rook('WR'), 0, 1)
+        to_x, to_y = 0, 2
+        result = self.board.grid[from_y][from_x].move(self.board, from_x, from_y, to_x, to_y)
+        self.assertEqual(result, "Invalid move", "Move should be identified as invalid.")
+        print("Test 7 for WR_move passed")
+
+         # Test 8: Makes sure you can't move when piece is in the way
+        from_x, from_y = 7, 0
+        self.board.place_piece(White_Rook('WR'), 5, 0)
+        to_x, to_y = 4, 0
+        result = self.board.grid[from_y][from_x].move(self.board, from_x, from_y, to_x, to_y)
+        self.assertEqual(result, "Invalid move", "Move should be identified as invalid.")
+        print("Test 8 for WR_move passed")
+
+if __name__ == '__main__':
+    unittest.main()
--- white_pieces/rook/white_rook.py
+++ white_pieces/rook/white_rook.py
@@ -15,11 +15,54 @@ class White_Rook(Piece):
         piece = board.grid[from_y][from_x]
         if piece and str(piece) == 'WR':
             # Ensure it's a valid move
-            if (from_x == to_x or from_y == to_y) and board.grid[to_y][to_x] is None:
-                board.grid[to_y][to_x] = piece
-                board.grid[from_y][from_x] = None
-                return "Valid move"
-        return "Invalid move"
+            #makes sure theres not a piece in the way
+            if (from_x != to_x) and (from_y != to_y):
+                return "Invalid move"
+            if (from_y == to_y):
+                x_or_y = from_x != to_x
+                change = from_x
+                #checks to see if we increment or decrement
+                if (change < to_x):
+                    value_change = 1
+                else:
+                    value_change = -1
+            else:
+                x_or_y = from_y != to_y
+                change = from_y #will be used to see to head toward to_y
+                #checks to see if we increment or decrement
+                if (change < to_y):
+                    value_change = 1
+                else:
+                    value_change = -1
+            while (from_x != to_x or from_y != to_y):
+                change += value_change
+                if (from_y == to_y):
+                    if (change == to_x) and (board.grid[to_y][to_x] is None):
+                        board.grid[to_y][to_x] = piece
+                        board.grid[from_y][from_x] = None
+                        return "Valid move"
+                        
+                    else:
+                        return "Invalid move"
+                else:
+                    if (change == to_y) and (board.grid[to_y][to_x] is None):
+                        board.grid[to_y][to_x] = piece
+                        board.grid[from_y][from_x] = None
+                        return "Valid move"
+                    
+                    
+                if (board.grid[change][from_x] is not None) :
+
+                    return "Invalid move"
+                
+                elif (board.grid[from_y][change] is not None):
+                    return "Invalid move"
+                
+
+            board.grid[to_y][to_x] = piece
+            board.grid[from_y][from_x] = None
+            return "Valid move"   
+            
     #how the piece can take a piece
     def take_piece(self, board, from_x, from_y, to_x, to_y):
         pass
