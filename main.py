5b72d2634326c67d057bbe27e82b6763ea915e82
Dan Collins <cdan200@icloud.com>
2024-08-09 20:08:25 -0600

    about to start WR take piece

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from helper import initialize_pieces
 def main():
     # Create a board
     board = Board()
-
     # Initialize and place pieces on the board
     initialize_pieces(board)
-
     # Print the board
     print(board)
 
--- tests/black_pieces_tests/rook/test_BR_move.py
+++ tests/black_pieces_tests/rook/test_BR_move.py
@@ -38,7 +38,17 @@ class TestBoard(unittest.TestCase):
         self.assertEqual(str(self.board.grid[to_y][to_x]), 'BR', "Target position should have the white Piece.")
         print("Test 1 for BR_move passed")
 
-        # Test 2: Move Piece forward by 1 square from non-initial position
+        # Test 2: Move Piece forward by 1 square from initial position
+        from_x, from_y = 7, 7
+        to_x, to_y = 6, 7
+        result = self.board.grid[from_y][from_x].move(self.board, from_x, from_y, to_x, to_y)
+        self.assertEqual(result, "Valid move", "Move should be identified as valid.")
+        self.assertIsNone(self.board.grid[from_y][from_x], "Original position should be empty after the move.")
+        self.assertIsNotNone(self.board.grid[to_y][to_x], "Target position should have the Piece after the move.")
+        self.assertEqual(str(self.board.grid[to_y][to_x]), 'BR', "Target position should have the white Piece.")
+        print("Test 2 for BR_move passed")
+
+        # Test 3: Move Piece forward by 1 square from non-initial position
         self.board.place_piece(Black_Rook('BR'), 2, 2)
         from_x, from_y = 2, 2
         to_x, to_y = 2, 7
@@ -47,10 +57,10 @@ class TestBoard(unittest.TestCase):
         self.assertIsNone(self.board.grid[from_y][from_x], "Original position should be empty after the move.")
         self.assertIsNotNone(self.board.grid[to_y][to_x], "Target position should have the Piece after the move.")
         self.assertEqual(str(self.board.grid[to_y][to_x]), 'BR', "Target position should have the white Piece.")
-        print("Test 2 for BR_move passed")
+        print("Test 3 for BR_move passed")
 
         
-        # Test 3: Move Piece to an occupied spot (invalid move)
+        # Test 4: Move Piece to an occupied spot (invalid move)
         self.board.place_piece(Black_Rook('BR'), 4, 4)
         self.board.place_piece(Black_Rook('BR'), 4, 5)  # Place another white Piece in front
         from_x, from_y = 4, 4
@@ -61,16 +71,16 @@ class TestBoard(unittest.TestCase):
         self.assertIsNotNone(self.board.grid[to_y][to_x], "Target position should still have the other Piece.")
         self.assertEqual(str(self.board.grid[from_y][from_x]), 'BR', "Original position should still have the white Piece.")
         self.assertEqual(str(self.board.grid[to_y][to_x]), 'BR', "Target position should still have the white Piece.")
-        print("Test 3 for BR_move passed")
+        print("Test 4 for BR_move passed")
 
-        # Test 4: Checks to see rook can't move diagonal
+        # Test 5: Checks to see rook can't move diagonal
         self.board.place_piece(Black_Rook('BR'), 4, 3)
         from_x, from_y = 4, 3
         to_x, to_y = 5, 4   # Invalid move as the spot is occupied
         result = self.board.grid[from_y][from_x].move(self.board, from_x, from_y, to_x, to_y)
         self.assertEqual(result, "Invalid move", "Move should be identified as valid.")
         self.assertIsNotNone(self.board.grid[from_y][from_x], "Original position should still have the Piece.")
-        print("Test 4 for BR_move passed")
+        print("Test 5 for BR_move passed")
 
 if __name__ == '__main__':
     unittest.main()
