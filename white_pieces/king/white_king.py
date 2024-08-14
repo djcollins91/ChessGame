576594c6fa33e2f37a32a281e8aca0f11bc4ddd1
Dan Collins <cdan200@icloud.com>
2024-08-13 21:54:54 -0600

    Made tests for WK_move

--- white_pieces/king/white_king.py
+++ white_pieces/king/white_king.py
@@ -12,7 +12,13 @@ class White_King(Piece):
 
     #how the piece move
     def move(self, board, from_x, from_y, to_x, to_y):
-        pass
+        piece = board.grid[from_y][from_x]
+        if piece and str(piece) == 'WK':
+            # Ensure it's a valid move
+            if (from_y == 1 and to_y in [2, 3]) or (from_y != 1 and to_y == from_y + 1):
+                if from_x == to_x and board.grid[to_y][to_x] is None:
+                    return Piece.valid_move(piece, board, from_x, from_y, to_x, to_y)
+        return Piece.invalid_move()
     
     #how the piece can take a piece
     def take_piece(self, board, from_x, from_y, to_x, to_y):
