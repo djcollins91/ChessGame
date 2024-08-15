b090f5e4d3d6e5b1ae4b4e3c84668c5ed2a303db
Dan Collins <cdan200@icloud.com>
2024-08-14 18:02:12 -0600

    made tests and made them pass for BQ

--- black_pieces/queen/black_queen.py
+++ black_pieces/queen/black_queen.py
@@ -48,6 +48,41 @@ class Black_Queen(Piece):
     
     #how the piece can take a piece
     def take_piece(self, board, from_x, from_y, to_x, to_y):
-        pass
+        piece = board.grid[from_y][from_x]
+        target = board.grid[to_y][to_x]
+        change_x = from_x
+        #if we don't move on the x cordinate
+        value_change_x = 0
+        change_y = from_y
+        #if we don't move on the y cordinate
+        value_change_y = 0
+
+        #when we do move on the spefic coordinates
+        if (change_x < to_x):
+            value_change_x = 1
+
+        if (change_x > to_x):
+            value_change_x = -1
+
+        if change_y < to_y:  
+            value_change_y = 1
+
+        if (change_y > to_y):
+            value_change_y = -1
+
+        if piece and str(piece) == 'BQ':
+            #makes sure it's a valid piece to take
+            if (target is not None) and str(target).startswith('W'):
+                #checks to make sure there isn't a piece in the way
+                while (change_y != to_y) or (change_x != to_x):
+                    change_y += value_change_y
+                    change_x += value_change_x
+                    #makes sure it's a our piece isn't in the way
+                    if ((board.grid[change_y][change_x]) is not None) and str(board.grid[change_y][change_x]).startswith('B'):
+                       return Piece.invalid_move()
+                
+                return Piece.valid_move(piece, board, from_x, from_y, to_x, to_y)
+            
+            return Piece.invalid_move()
 
     
