2d6c7ff485e35f2fe6f0af38914f80c35e9776d5
Dan Collins <cdan200@icloud.com>
2024-08-16 21:14:59 -0600

    Added player classes

---
+++ computer_player.py
@@ -0,0 +1,9 @@
+from player import Player
+
+
+class Computer_Player(Player):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
---
+++ human_player.py
@@ -0,0 +1,9 @@
+from player import Player
+
+
+class Human_Player(Player):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
