4b21e102c05d9e1de2d32a4d346083f200815c2d
Dan Collins <cdan200@icloud.com>
2025-05-15 23:29:52 -0600

    Got rid of some unneccsary files

--- player.py
+++
@@ -1,9 +0,0 @@
-class Player:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
-    
-    def turn(self):
-        raise NotImplementedError("This method should be overridden by subclasses")
